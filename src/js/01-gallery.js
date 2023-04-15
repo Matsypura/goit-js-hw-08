a817603c69fc5741951ebaa872697b21a48c555e
Matsypura <117666348+Matsypura@users.noreply.github.com>
2023-04-15 12:49:04 +0300

    Готова робота

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -42,33 +42,3 @@ const slideOriginalImg = new SimpleLightbox('.gallery a', {
 
 
 
-
-
-
-
-
-
-
-
-// createGalleryMarkup(galleryItems, galleryElement);
-
-// function createGalleryMarkup(items, container) {
-//     const markup = items
-//       .map(
-//         ({ preview, original, description }) =>
-//           `<li class='gallery__item'>
-//           <a class='gallery__link' href='${original}'>
-//             <img
-//               class='gallery__image'
-//               src='${preview}'
-//               data-source='${original}'
-//               alt='${description}'
-//             />
-//           </a>
-//         </li>`
-//       )
-//       .join('');
-//     container.insertAdjacentHTML('beforeend', markup);
-//   }
-
-
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const KEY_DATA_STORAGE = 'feedback-form-state';
+const parsedSettings = JSON.parse(localStorage.getItem(KEY_DATA_STORAGE));
+
+let formData = parsedSettings ?? {};
+
+refs.form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+getSavedSettings();
+
+function getSavedSettings() {
+  const { email, message } = refs.form.elements;
+  email.value = formData.email ?? '';
+  message.value = formData.message ?? '';
+}
+
+function onInputForm(e) {
+  const currentEl = e.target;
+  formData[currentEl.name] = currentEl.value;
+  localStorage.setItem(KEY_DATA_STORAGE, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log('data:', formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY_DATA_STORAGE);
+  formData = {};
+}
